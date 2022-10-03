5fd705cf4d564514a529f205cbf7b79482c2bb75
ugik <ugik.dev@gmail.com>
2022-10-03 15:19:48 +0700

    loc

--- application/views/my/absen_record.php
+++ application/views/my/absen_record.php
@@ -34,18 +34,12 @@
     </div>
     </form>
 </div>
-</div>
 
 <script>
     $(document).ready(function() {
         $('#sidebar_wrapper_func').addClass('close_icon');
         $('#sidebar_skp').addClass('active_c');
     });
-</script>
-
-
-<script>
-
 </script>
 
 
@@ -99,27 +93,27 @@
             "<br>Longitude: " + position.coords.longitude +
             "<br>ss: " + position.coords.latitude + ', ' + position.coords.longitude;
 
-        // var map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 13);
+        var map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 13);
 
-        // var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        //     maxZoom: 19,
-        // }).addTo(map);
+        var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+        }).addTo(map);
 
-        // var marker = L.marker([51.5, -0.09]).addTo(map);
+        var marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
 
-        // var circle = L.circle([51.508, -0.11], {
-        //     color: 'red',
-        //     fillColor: '#f03',
-        //     fillOpacity: 0.5,
-        //     radius: 500
-        // }).addTo(map);
+        var circle = L.circle([51.508, -0.11], {
+            color: 'red',
+            fillColor: '#f03',
+            fillOpacity: 0.5,
+            radius: 500
+        }).addTo(map);
 
 
-        // var polygon = L.polygon([
-        //     [51.509, -0.08],
-        //     [51.503, -0.06],
-        //     [51.51, -0.047]
-        // ]).addTo(map);
+        var polygon = L.polygon([
+            [51.509, -0.08],
+            [51.503, -0.06],
+            [51.51, -0.047]
+        ]).addTo(map);
 
     }
 </script>
