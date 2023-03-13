62b014db12dc1e22666fd2b1eed5f127bcd051e0
Ugik Dev <ugik.dev@gmail.com>
2023-03-13 15:39:12 +0700

    asd

--- application/models/CrossModel.php
+++ application/models/CrossModel.php
@@ -9,7 +9,7 @@ class CrossModel extends CI_Model
         $DB2 = $this->load->database('puskesmas', TRUE);
         $DB2->select('pkm,tulisan_id,tulisan_judul,tulisan_tanggal,tulisan_slug,tulisan_jenis');
         $DB2->from('v_tbl_tulisan');
-        $DB2->limit('10');
+        $DB2->limit('50');
         $DB2->where('tulisan_tanggal is not null');
         $DB2->where('tulisan_tanggal <> "0000-00-00"');
         if (!empty($filter['dari'])) $DB2->where('tulisan_tanggal >="' . $filter['dari'] . '"');
